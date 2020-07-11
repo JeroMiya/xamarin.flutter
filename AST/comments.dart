d9bfdc32b75d7e994c0ea9894faf3504acc70ab9
Jeremy Bell <bell.jeremy@gmail.com>
2020-07-11 17:59:09 -0400

    Re-enabled comments in the generated output.

--- AST/comments.dart
+++ AST/comments.dart
@@ -2,7 +2,6 @@ import 'package:analyzer/dart/element/element.dart';
 
 class Comments {
   static void appendComment(StringBuffer buffer, Element element) {
-    return;
     var dartComment = element.documentationComment;
     if (dartComment == null || dartComment == "") return;
 
